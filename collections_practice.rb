876d09f9fdab78c8be5f42070b317d5acada7f09
rahimshahad <rahim.shahad2@gmail.com>
2020-12-17 14:17:44 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(integers)
+    integers.sort do |a, b|
+        a<=>b
+    end
+end
+
+def sort_array_desc(integers)
+    integers.sort do |a, b|
+        b<=>a
+    end
+end
+
+def sort_array_char_count(string)
+    string.sort do |a, b|
+        a.length<=>b.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |string|
+        string[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |array|
+        array.start_with?("a")
+    end
+end
+
+def sum_array(integers)
+    integers.inject(0) do |sum, x|
+        sum + x
+    end
+end
+
+def add_s(array)
+ array.map do |word|
+     if array[1] == word
+         word
+     else
+         word << "s"
+        end 
+    end
+end
 
